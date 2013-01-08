204c7780a6a022a7ba9f22204090b05daea5c318
Michael Klein <mbklein@gmail.com>
2013-01-08 15:20:11 -0600

    Remove hostname

--- lib/omniauth/strategies/nuldap.rb
+++ lib/omniauth/strategies/nuldap.rb
@@ -1,8 +1,8 @@
+require 'omniauth-ldap'
 module OmniAuth
 	module Strategies
 		class NULDAP < LDAP
 	    option :title, "Northwestern NetID"
-	    option :host, "pesto.library.northwestern.edu"
 	    option :base, "ou=people,dc=northwestern,dc=edu"
 	    option :uid, "uid"
 		end
